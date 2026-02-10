d434788fbb1a86f3bbaa200829b89cb917f36f09
JimCosta <jimmyhorseman95@gmail.com>
2026-02-10 00:45:34 -0300

    produccion

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ app.use("/api/products", productsRoutes);
 app.use("/api/sales", salesRoutes);
 app.use("/dashboard", dashboardRoutes);
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto", PORT);
 });
 
 app.get("/", (req, res) => {
